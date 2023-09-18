2e9a8a1c8e4313806bf46e539da99b1594fd7f6e
juhee067 <66045537+juhee067@users.noreply.github.com>
2023-09-18 13:31:02 +0900

    Time: 43 ms (96.7%), Space: 43.6 MB (81.62%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -3,18 +3,9 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-   let result=[];
 // 1. 문자를 " " 기준으로 나눈다.
 // 2. " " 나눈 배열에 포함 되어있는 " "를 모두 없앤다.
 // 3. 문자만 남은 배열로 역순으로 나열한다.
-const nonSpaceArr = s.split(' ').filter((item)=> item!=="").reverse();
 // 4. 단어와 단어 사이에 " "을 포함시켜준다.
-for(let i=0; i<nonSpaceArr.length; i++){
-    if(i === nonSpaceArr.length - 1){
-        result.push(nonSpaceArr[i]);
-        return result.join('')
-    }
-result.push(nonSpaceArr[i] + " ");
-}
-return result.join('')
+return s.split(' ').filter((item)=> item!=="").reverse().join(' ')
 };
